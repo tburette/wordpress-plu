db69d7fbbf0fa59b93401d551498970922f33c0b
Thomas Burette <burettethomas@gmail.com>
2026-09-07 22:54:35 +0200

    Add inline base padding styles to patterns and templates so editor block validation passes

--- plugins/lpu-split-section/lpu-split-section.php
+++ plugins/lpu-split-section/lpu-split-section.php
@@ -390,7 +390,7 @@ function lpu_split_section_register_patterns()
 <div class="wp-block-lpu-split-section alignfull lpu-split-v2">
 	<!-- wp:lpu/split-zone {"side":"left","frame":"ecru","mediaFill":false} -->
 	<div class="wp-block-lpu-split-zone lpu-split-v2__zone lpu-split-v2__zone--left lpu-split-v2__zone--frame-ecru">
-		<!-- wp:group {"className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":"var:preset|spacing|xl"},"@tablet":{"spacing":{"padding":"var:preset|spacing|lg"}},"@mobile":{"spacing":{"padding":"var:preset|spacing|lg"}}} -->
+		<!-- wp:group {"className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"@tablet":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}},"@mobile":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}}} -->
 		<div class="wp-block-group lpu-split-v2__inset">
 			<!-- wp:paragraph {"fontFamily":"oswald","fontSize":"text","className":"lpu-eyebrow"} -->
 			<p class="lpu-eyebrow has-oswald-font-family has-text-font-size">Sur-titre</p>
@@ -410,7 +410,7 @@ function lpu_split_section_register_patterns()
 
 	<!-- wp:lpu/split-zone {"side":"right","frame":"motif-4","mediaFill":false} -->
 	<div class="wp-block-lpu-split-zone lpu-split-v2__zone lpu-split-v2__zone--right lpu-split-v2__zone--frame-motif-4">
-		<!-- wp:group {"backgroundColor":"ecru","className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":"var:preset|spacing|xl"},"@tablet":{"spacing":{"padding":"var:preset|spacing|lg"}},"@mobile":{"spacing":{"padding":"var:preset|spacing|lg"}}} -->
+		<!-- wp:group {"backgroundColor":"ecru","className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"@tablet":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}},"@mobile":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}}} -->
 		<div class="wp-block-group lpu-split-v2__inset has-ecru-background-color has-background">
 			<!-- wp:paragraph {"fontFamily":"oswald","fontSize":"text","className":"lpu-eyebrow"} -->
 			<p class="lpu-eyebrow has-oswald-font-family has-text-font-size">Zone droite</p>
@@ -478,7 +478,7 @@ HTML,
 <div class="wp-block-lpu-split-section alignfull lpu-split-v2">
 	<!-- wp:lpu/split-zone {"side":"left","frame":"motif-7","mediaFill":false} -->
 	<div class="wp-block-lpu-split-zone lpu-split-v2__zone lpu-split-v2__zone--left lpu-split-v2__zone--frame-motif-7">
-		<!-- wp:group {"backgroundColor":"ecru","className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":"var:preset|spacing|xl"},"@tablet":{"spacing":{"padding":"var:preset|spacing|lg"}},"@mobile":{"spacing":{"padding":"var:preset|spacing|lg"}}} -->
+		<!-- wp:group {"backgroundColor":"ecru","className":"lpu-split-v2__inset","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"@tablet":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}},"@mobile":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}}}} -->
 		<div class="wp-block-group lpu-split-v2__inset has-ecru-background-color has-background">
 			<!-- wp:paragraph {"fontFamily":"oswald","fontSize":"text","className":"lpu-eyebrow"} -->
 			<p class="lpu-eyebrow has-oswald-font-family has-text-font-size">Sur-titre</p>
--- themes/lepaysanurbain/patterns/cards.php
+++ themes/lepaysanurbain/patterns/cards.php
@@ -9,7 +9,7 @@
  */
 ?>
 <!-- wp:group {"align":"full","backgroundColor":"ecru","className":"lpu-band lpu-card-grid lpu-motif lpu-motif-1-bandeau","layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|lg","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|lg"}},"@tablet":{"spacing":{"padding":{"right":"var:preset|spacing|md","left":"var:preset|spacing|md"}}},"@mobile":{"spacing":{"padding":{"right":"var:preset|spacing|md","left":"var:preset|spacing|md"}}}}} -->
-<div class="wp-block-group alignfull lpu-band lpu-card-grid lpu-motif lpu-motif-1-bandeau has-ecru-background-color has-background">
+<div class="wp-block-group alignfull lpu-band lpu-card-grid lpu-motif lpu-motif-1-bandeau has-ecru-background-color has-background" style="padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--lg)">
 	<!-- wp:heading {"align":"wide","level":2,"fontSize":"subtitle","className":"lpu-card-grid__title"} -->
 	<h2 class="wp-block-heading alignwide lpu-card-grid__title has-subtitle-font-size">Titre de la grille</h2>
 	<!-- /wp:heading -->
@@ -23,7 +23,7 @@
 			<!-- /wp:image -->
 
 			<!-- wp:group {"className":"lpu-card__body","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"0"}}}} -->
-			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical">
+			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical" style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:0">
 				<!-- wp:heading {"level":3,"fontFamily":"oswald","fontSize":"text"} -->
 				<h3 class="wp-block-heading has-oswald-font-family has-text-font-size">Titre de carte 1</h3>
 				<!-- /wp:heading -->
@@ -33,9 +33,9 @@
 				<!-- /wp:paragraph -->
 
 				<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm"}}}} -->
-				<div class="wp-block-buttons">
+				<div class="wp-block-buttons" style="padding-top:var(--wp--preset--spacing--sm)">
 					<!-- wp:button {"style":{"spacing":{"padding":{"top":"0.4rem","right":"0.85rem","bottom":"0.4rem","left":"0.85rem"}}}} -->
-					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">En savoir plus</a></div>
+					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-top:0.4rem;padding-right:0.85rem;padding-bottom:0.4rem;padding-left:0.85rem">En savoir plus</a></div>
 					<!-- /wp:button -->
 				</div>
 				<!-- /wp:buttons -->
@@ -51,7 +51,7 @@
 			<!-- /wp:image -->
 
 			<!-- wp:group {"className":"lpu-card__body","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"0"}}}} -->
-			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical">
+			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical" style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:0">
 				<!-- wp:heading {"level":3,"fontFamily":"oswald","fontSize":"text"} -->
 				<h3 class="wp-block-heading has-oswald-font-family has-text-font-size">Titre de carte 2</h3>
 				<!-- /wp:heading -->
@@ -61,9 +61,9 @@
 				<!-- /wp:paragraph -->
 
 				<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm"}}}} -->
-				<div class="wp-block-buttons">
+				<div class="wp-block-buttons" style="padding-top:var(--wp--preset--spacing--sm)">
 					<!-- wp:button {"style":{"spacing":{"padding":{"top":"0.4rem","right":"0.85rem","bottom":"0.4rem","left":"0.85rem"}}}} -->
-					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">En savoir plus</a></div>
+					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-top:0.4rem;padding-right:0.85rem;padding-bottom:0.4rem;padding-left:0.85rem">En savoir plus</a></div>
 					<!-- /wp:button -->
 				</div>
 				<!-- /wp:buttons -->
@@ -79,7 +79,7 @@
 			<!-- /wp:image -->
 
 			<!-- wp:group {"className":"lpu-card__body","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"0"}}}} -->
-			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical">
+			<div class="wp-block-group lpu-card__body is-layout-flex is-vertical" style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:0">
 				<!-- wp:heading {"level":3,"fontFamily":"oswald","fontSize":"text"} -->
 				<h3 class="wp-block-heading has-oswald-font-family has-text-font-size">Titre de carte 3</h3>
 				<!-- /wp:heading -->
@@ -89,9 +89,9 @@
 				<!-- /wp:paragraph -->
 
 				<!-- wp:buttons {"style":{"spacing":{"padding":{"top":"var:preset|spacing|sm"}}}} -->
-				<div class="wp-block-buttons">
+				<div class="wp-block-buttons" style="padding-top:var(--wp--preset--spacing--sm)">
 					<!-- wp:button {"style":{"spacing":{"padding":{"top":"0.4rem","right":"0.85rem","bottom":"0.4rem","left":"0.85rem"}}}} -->
-					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">En savoir plus</a></div>
+					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-top:0.4rem;padding-right:0.85rem;padding-bottom:0.4rem;padding-left:0.85rem">En savoir plus</a></div>
 					<!-- /wp:button -->
 				</div>
 				<!-- /wp:buttons -->
