76d143aa430f51a843c17087752de63cc56754b4
Thomas Burette <burettethomas@gmail.com>
2026-09-12 08:44:14 +0200

    refactor: remove unnecessary class from image elements

--- plugins/lpu-split-section/patterns/split-motif-image.php
+++ plugins/lpu-split-section/patterns/split-motif-image.php
@@ -16,8 +16,8 @@
 <!-- /wp:lpu/split-zone -->
 
 <!-- wp:lpu/split-zone {"side":"right","frame":"none","mediaFill":true,"className":"wp-block-lpu-split-zone","layout":{"type":"constrained"}} -->
-<div class="lpu-split-v2__zone lpu-split-v2__zone--right lpu-split-v2__zone--frame-none lpu-split-v2__zone--media-fill wp-block-lpu-split-zone"><!-- wp:image {"linkDestination":"none","className":"lpu-media-placeholder"} -->
-<figure class="wp-block-image lpu-media-placeholder"><img src="/wp-content/themes/lepaysanurbain/assets/images/pattern-placeholder.svg" alt=""/></figure>
+<div class="lpu-split-v2__zone lpu-split-v2__zone--right lpu-split-v2__zone--frame-none lpu-split-v2__zone--media-fill wp-block-lpu-split-zone"><!-- wp:image {"linkDestination":"none"} -->
+<figure class="wp-block-image"><img src="/wp-content/themes/lepaysanurbain/assets/images/pattern-placeholder.svg" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:lpu/split-zone --></div>
 <!-- /wp:lpu/split-section -->
